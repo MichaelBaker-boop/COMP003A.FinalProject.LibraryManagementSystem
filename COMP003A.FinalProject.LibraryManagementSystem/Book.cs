b40cb9d8d03f6e6c171d0c347baadcd4f920ebc8
michaelbaker-boop <michaelbaker4@my.whccd.edu>
2025-03-02 13:58:21 -0800

    Add project files.

---
+++ COMP003A.FinalProject.LibraryManagementSystem/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.FinalProject.LibraryManagementSystem
+{
+    public abstract class Book
+    {
+        // Fields
+        private string _bookTitle;
+        private string _bookAuthor;
+        private string _bookISBN;
+
+
+        // Properties
+        public string Title
+        {
+            get
+            {
+                return _bookTitle;
+            }
+            set
+            {
+                _bookTitle = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return _bookAuthor;
+            }
+            set
+            {
+                _bookAuthor = value;
+            }
+        }
+
+        public string ISBN
+        {
+            get
+            {
+                return _bookISBN;
+            }
+            set
+            {
+                _bookISBN = value;
+            }
+        }
+
+        public abstract void DisplayBookInfo();
+
+    }
+}
---
+++ COMP003A.FinalProject.LibraryManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.FinalProject.LibraryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
