db0f351d4cbbfb670ad5785f3030d7d66f82b21e
michaelbaker-boop <michaelbaker4@my.whccd.edu>
2025-03-02 14:50:30 -0800

    Added main menu

--- COMP003A.FinalProject.LibraryManagementSystem/Program.cs
+++ COMP003A.FinalProject.LibraryManagementSystem/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> ownedBooks = new List<Book>();
+            List<Book> unavailableBooks = new List<Book>();
+            int menuSelection = 0;
+            Console.WriteLine("Weclome to the library management system!");
+
+            bool menu = true;
+            while (menu == true)
+            {
+                Console.WriteLine("Please choose an option: \n");
+                Console.WriteLine("1) Add a new book.");
+                Console.WriteLine("2) Remove an old book");
+                Console.WriteLine("3) Check out a book");
+                Console.WriteLine("4) Return a book");
+                Console.WriteLine("5) Display available books");
+                Console.WriteLine("6) Display unavailable books");
+                Console.WriteLine("7) Exit");
+
+                try
+                {
+                    menuSelection = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Input invalid, must be a number");
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
