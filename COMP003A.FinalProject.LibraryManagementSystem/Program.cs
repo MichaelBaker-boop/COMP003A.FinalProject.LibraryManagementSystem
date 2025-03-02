471ff18b6ded45c60890cd0ebbf96f8b76a8fa68
michaelbaker-boop <michaelbaker4@my.whccd.edu>
2025-03-02 15:14:01 -0800

    Added first option program

--- COMP003A.FinalProject.LibraryManagementSystem/Program.cs
+++ COMP003A.FinalProject.LibraryManagementSystem/Program.cs
@@ -29,6 +29,68 @@
                 {
                     Console.WriteLine("Input invalid, must be a number");
                     Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                switch (menuSelection)
+                {
+                    case 1:
+                        Console.WriteLine("You are currently adding a book to the system.");
+                        bool validInput = false; //Initialize test of user input
+                        Book newBook = new PrintedBook("", "", ""); // Create a new printed book object
+                        while (!validInput) // While loop to verify input
+                        {
+                            Console.Write("Enter the title of the book: ");
+                            newBook.Title = Console.ReadLine();
+                            if (string.IsNullOrEmpty(newBook.Title))
+                            {
+                                Console.WriteLine("The title cannot be empty. Please try again.");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        while (!validInput)
+                        {
+                            Console.WriteLine("Enter the book author: ");
+                            newBook.Author = Console.ReadLine();
+                            if (string.IsNullOrEmpty(newBook.Author))
+                            {
+                                Console.WriteLine("The author cannot be empty, please try again.");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        while (!validInput)
+                        {
+                            Console.WriteLine("Enter the book ISBN: ");
+                            newBook.ISBN = Console.ReadLine();
+                            if (string.IsNullOrEmpty(newBook.ISBN))
+                            {
+                                Console.WriteLine("The ISBN cannot be empty");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine($"{newBook.Title}, {newBook.Author}, {newBook.ISBN}");
+                        ownedBooks.Add(newBook);
+                        Console.WriteLine("Book added successfully!\n");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("You are currently removing a book from the system");
+
+      
+                   
+                    
                 }
             }
         }
